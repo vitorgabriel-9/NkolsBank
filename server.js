d54d773f8b55c3fad8dbadb375827b5b3acf72d3
vitorgabriel-9 <vitor.gabriel09@aluno.ifce.edu.br>
2026-06-09 10:50:57 -0300

    Correção de Bugs - Site quebrado no HTML e CSS

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static("public"));
 // ==========================
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "login.html"));
 });
 
 // ==========================
